ac7a2a942f0f4951776cd4190087634fb83946ea
= <=>
2021-12-24 18:54:37 +0700

    deploy

--- php/src/connection.php
+++ php/src/connection.php
@@ -1,4 +1,4 @@
 <?php
-$con=mysqli_connect("localhost","root","","toko_online") or die(mysqli_error($con));
+$con=mysqli_connect("10.0.0.223","admin","123@Imel123","store") or die(mysqli_error($con));
 //$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
 ?>
